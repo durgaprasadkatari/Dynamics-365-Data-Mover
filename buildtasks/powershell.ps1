e3121b992d5144a5232c7203a2a71d728fe8b857
Durgaprasad Katari <durgaprasadkatari@gmail.com>
2019-07-18 17:31:43 +0530

    Modified code to work for any fetchxml

--- buildtasks/powershell.ps1
+++ buildtasks/powershell.ps1
@@ -47,20 +47,27 @@ try {
     $targetConn = Connect-CrmOnline -Credential $targetCred -ServerUrl $TargetUrl
         
     Write-Host "Connected to target Dynamics 365" $targetConn.CrmConnectOrgUriActual.AbsoluteUri
+    $xml = [xml]$sourceRecords.FetchXml
+    $atts = $xml.GetElementsByTagName('attribute');
+    $entityName = $xml.GetElementsByTagName('entity').name;
+    $createdRecordCount = 0
+    $updatedRecordCount = 0
     foreach($entity in $sourceRecords.CrmRecords)
     {
-        $createdRecordCount = 0
-        $updatedRecordCount = 0
-        $apiEntity = New-Object Microsoft.Xrm.Sdk.Entity("ft_api")
-        $apiEntity.Id = $entity.ft_apiid
-        $apiEntity.Attributes["ft_name"] = $entity.ft_name
-        $apiEntity.Attributes["ft_url"] = $entity.ft_url
-        $apiEntity.Attributes["ft_apikey"] = $entity.ft_apikey
-        
-        $statecode_code_os = New-Object Microsoft.Xrm.Sdk.OptionSetValue($entity.statecode_Property.Value.Value)
-        $apiEntity.Attributes["statecode"] = [Microsoft.Xrm.Sdk.OptionSetValue] $statecode_code_os
-        $statuscode_code_os = New-Object Microsoft.Xrm.Sdk.OptionSetValue($entity.statuscode_Property.Value.Value)
-        $apiEntity.Attributes["statuscode"] = [Microsoft.Xrm.Sdk.OptionSetValue] $statuscode_code_os
+        $apiEntity = New-Object Microsoft.Xrm.Sdk.Entity($entityName)
+        foreach($att in $atts.name)
+        {
+            $att1 = $att+"_Property";
+            if($att -eq $entityName+"id")
+            {
+                $entityId = $entityName+"id";
+                $apiEntity.Id = $entity.$entityId
+            }
+            else {
+                $apiEntity.Attributes[$att] = $entity.$att1.Value
+            }           
+        }
+
         $request = new-object Microsoft.Xrm.Sdk.Messages.UpsertRequest
         $request.Target = $apiEntity
         $response = $targetConn.Execute($request)
